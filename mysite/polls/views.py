726bdc885d8a71da49f6801b8fb2cd7f38277531
Viet Duc Do <duc.do32@mailbox.org>
2024-04-19 20:40:17 +1200

    shortcut get_object_or_404 in view.detail

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -15,12 +15,9 @@ def index(request):
 
 
 def detail(request, question_id):
-	try:
-		question = Question.objects.get(pk=question_id)
-	except Question.DoesNotExist:
-		raise Http404("Question does not exist")
+	question = get_object_or_404(Question, pk=question_id)
 	return render(request, "polls/detail.html", {"question": question})
-
+	
 
 def results(request, question_id):
 	response = "You are looking at the results of question %s. "
