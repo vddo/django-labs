25b4f96aa88539b42f53d8de6f72b9f086d07504
Viet Duc Do <duc.do32@mailbox.org>
2024-04-20 14:06:06 +1200

    Trying out generic views

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,7 @@
-from django.http import Http404
+from django.db.models import F
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
-from django.template import loader
+from django.urls import reverse
 
 from .models import Choice, Question
 
@@ -40,5 +40,5 @@ def vote(request, question_id):
 	else:
 		selected_choice.votes = F("votes") + 1
 		selected_choice.save()
-		return HttpResponseRedirect(reverse("polls:results", args=(question.id)))
+		return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
 
